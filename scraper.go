8e8147017a4d5f2f6b51399496fbc68491557889
jsantana-h <jjhurtado07@gmail.com>
2023-05-22 01:03:31 -0400

    Basic scraping

--- scraper.go
+++ scraper.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 
 	// Importing Colly
 	"github.com/gocolly/colly"
 )
 
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
 func main() {
 	c := colly.NewCollector()
+
+	var pokemonProducts []PokemonProduct
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL)
@@ -23,9 +31,19 @@ func main() {
 		fmt.Println("Page visited: ", r.Request.URL)
 	})
 
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		// printing all URLs associated with the a links in the page
-		fmt.Println("-", e.Attr("href"))
+	// iterating over the list of HTML product elements
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		// initializing a new PokemonProduct instance
+		pokemonProduct := PokemonProduct{}
+
+		// scraping the data of interest
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		// adding the product instance with scraped data to the list of products
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
@@ -34,6 +52,41 @@ func main() {
 
 	// downloading the target HTML page
 	c.Visit("https://scrapeme.live/shop/")
+
+	// opening the CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	// initializing a file writer
+	writer := csv.NewWriter(file)
+
+	// defining the CSV headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	// writing the column headers
+	writer.Write(headers)
+
+	// adding each Pokemon product to the CSV output file
+	for _, pokemonProduct := range pokemonProducts {
+		// converting a PokemonProduct to an array of strings
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+
+		// writing a new CSV record
+		writer.Write(record)
+	}
+	defer writer.Flush()
 
 	fmt.Println("Hello, World!")
 }
